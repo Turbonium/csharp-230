323fe88e4ff30bd2714ae837ec36b3f889e0da9b
DESKTOP-E55BUP2\Turbo <mgkett@gmail.com>
2017-04-13 17:51:32 -0700

    Hello World checkin

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models; // add to access models
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Products()
+        {
+            var products = new Product[]
+            {
+            // as part of exercise add a value for Count for each product
+        new Product{ ProductId = 1, Name = "First One", Price = 1.11m, Count = 0},
+        new Product{ ProductId = 2, Name="Second One", Price = 2.22m, Count = 3},
+        new Product{ ProductId = 3, Name="Third One", Price = 3.33m, Count = 1},
+        new Product{ ProductId = 4, Name="Fourth One", Price = 4.44m, Count = 10},
+            };
+
+            return View(products);
+        }
+        public ActionResult Product()
+        {
+            var myProduct = new Product
+            {
+                ProductId = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "water-sports",
+                Price = 200m,
+            };
+
+            return View(myProduct);
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(Models.GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid) // check on validation
+            {
+                return View("Thanks", guestResponse);
+            }
+            else //if false stay on the same page
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; // to access validation attribute
+
+namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        
+        //Exercise: Add validation attribute for email
+        [Required(ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; } //exercise: add the email
+
+        //Exercise: Add validation attribute for phone
+        [Required(ErrorMessage = "Please enter your phone")]
+        public string Phone { get; set; }
+
+        //Exercise: Add validation attribute for attendance
+        [Required(ErrorMessage = "Please enter if you will attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
