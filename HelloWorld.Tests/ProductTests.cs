e57edd7dad6133fdb2358ed166b65aca31fbd8ee
DESKTOP-E55BUP2\Turbo <mgkett@gmail.com>
2017-04-13 20:53:38 -0700

    Added Unit Tests

---
+++ HelloWorld.Tests/ProductTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld.Models;
+using HelloWorld.Controllers;
+using Moq;
+
+namespace HelloWorld.Tests
+{
+    [TestClass]
+    public class ProductTests
+    {
+        [TestMethod]
+        public void TestMethodWithFakeClass()
+        {
+            // Arrange
+            var controller = new HomeController(new FakeProductRepository());
+
+            // Act
+            var result = controller.Products();
+
+            // Assert
+            var products = (Product[])((System.Web.Mvc.ViewResultBase)(result)).Model;
+            Assert.AreEqual(4, products.Length, "Length is invalid");
+        }
+
+        [TestMethod]
+        public void TestMethodWithMoq()
+        {
+            var mockProductRepository = new Mock<IProductRepository>();
+            mockProductRepository
+                .SetupGet(t => t.Products)
+                .Returns(() =>
+                {
+                    return new Product[]{
+                    new Product{ ProductId=101, Name="Baseball", Description="balls", Price=14.20m},
+                    new Product{ ProductId=102, Name="Football", Description="nfl", Price=9.24m}
+                    };
+                });
+
+            // Arrange
+            var controller = new HomeController(mockProductRepository.Object);
+
+            // Act
+            var result = controller.Products();
+
+            // Assert
+            var products = (Product[])((System.Web.Mvc.ViewResultBase)(result)).Model;
+            Assert.AreEqual(2, products.Length, "Length is invalid");
+        }
+    }
+}
